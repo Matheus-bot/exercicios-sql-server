708a254795c0f4a97c76b4c2b71527e2f2baa6a1
Matheus <matheushenrry00@gmail.com>
2026-08-17 19:31:06 -0300

    feat: adiciona exercícios 04 e 05

--- exercicios/01-Fundamentos/exercicios.sql
+++ exercicios/01-Fundamentos/exercicios.sql
@@ -1,5 +1,19 @@
--- Exercício 01 - Liste todos os clientes cadastrados na tabela dim_cliente
+-- Exercício 01 - Liste todos os clientes cadastrados na tabela dim_cliente ?
 
--- Exercicio 02 - Liste apenas os nomes e categorias dos produtos
+-- Exercicio 02 - Liste apenas os nomes e categorias dos produtos ?
 
--- Exercício 03 - Quantos clientes existem na base
+-- Exercício 03 - Quantos clientes existem na base ?
+
+-- Exercício 04 - Quantos produtos existem cadastrados ?
+
+-- Exercício 05 - Qual o valor total de faturamento da empresa?
+
+-- Exercício 06 - Qual o ticket médio das vendas?
+
+-- Exercício 07 - Liste todas as lojas cadastradas.
+
+-- Exercício 08 - Liste todos os funcionários e seus cargos.
+
+-- Exercício 09 - Mostre as 10 primeiras vendas realizadas.
+
+-- Exercício 10 - Mostre o maior valor de venda realizado.
--- exercicios/01-Fundamentos/solucao.sql
+++ exercicios/01-Fundamentos/solucao.sql
@@ -1,9 +1,7 @@
 -- Exercício 01 - Liste todos os clientes cadastrados na tabela dim_cliente
 
-SELECT 
-    nome_cliente
-FROM 
-    dim_cliente;
+SELECT nome_cliente
+FROM  dim_cliente;
 
 -- Exercício 02 - Liste apenas os nomes e categorias dos produtos
 
@@ -13,3 +11,13 @@ SELECT nome_produto, categoria
 -- Exercício 03  Quantos clientes existem na base
 SELECT count(*)
   FROM dim_cliente;
+
+-- Exercício 04  Quantos produtos existem cadastrados ?
+
+SELECT COUNT(nome_produto) AS TOTAL_PRODUTOS
+FROM dim_produto
+
+-- Exercício 05 - Qual o valor total de faturamento da empresa?
+
+SELECT ROUND(SUM(valor_total),2)AS FATURAMENTO_TOTAL
+FROM  fato_vendas;
