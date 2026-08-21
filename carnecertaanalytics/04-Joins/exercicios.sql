785962474bfc77aebd12f8e59a0046a634e1370a
Matheus <matheushenrry00@gmail.com>
2026-08-21 17:09:06 -0300

    feat: adiciona exercício 02 Joins

--- carnecertaanalytics/04-Joins/exercicios.sql
+++ carnecertaanalytics/04-Joins/exercicios.sql
@@ -1,1 +1,3 @@
 -- 01 Mostre o faturamento por loja.
+ 
+-- 02 Mostre a quantidade de vendas por loja.
--- carnecertaanalytics/04-Joins/solucao.sql
+++ carnecertaanalytics/04-Joins/solucao.sql
@@ -8,3 +8,12 @@ FROM fato_vendas a
 INNER JOIN dim_loja b 
 ON  a.id_loja = B.id_loja
 GROUP BY nome_loja 
+
+-- 02 Mostre a quantidade de vendas por loja.
+SELECT
+    l.nome_loja,
+    COUNT(v.id_venda)
+FROM fato_vendas v
+JOIN dim_loja l
+ON v.id_loja=l.id_loja
+GROUP BY l.nome_loja;
