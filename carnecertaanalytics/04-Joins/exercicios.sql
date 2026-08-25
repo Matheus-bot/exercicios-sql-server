92c0c265c3ef90954d3a4e6ded754136a969518d
Matheus <matheushenrry00@gmail.com>
2026-08-24 23:00:21 -0300

    feat: adiciona exercício 03 Joins

--- carnecertaanalytics/04-Joins/exercicios.sql
+++ carnecertaanalytics/04-Joins/exercicios.sql
@@ -1,3 +1,7 @@
 -- 01 Mostre o faturamento por loja.
  
 -- 02 Mostre a quantidade de vendas por loja.
+
+-- 03 Mostre quantos funcionários existem em cada loja.
+
+
--- carnecertaanalytics/04-Joins/solucao.sql
+++ carnecertaanalytics/04-Joins/solucao.sql
@@ -10,6 +10,7 @@ ON  a.id_loja = B.id_loja
 GROUP BY nome_loja 
 
 -- 02 Mostre a quantidade de vendas por loja.
+
 SELECT
     l.nome_loja,
     COUNT(v.id_venda)
@@ -17,3 +18,12 @@ FROM fato_vendas v
 JOIN dim_loja l
 ON v.id_loja=l.id_loja
 GROUP BY l.nome_loja;
+
+-- 03 Mostre quantos funcionários existem em cada loja.
+
+SELECT 
+    l.nome_loja AS LOJA,
+    COUNT(f.id_funcionario) AS FUNCIONARIOS
+FROM dim_funcionario f
+JOIN dim_loja l ON f.id_loja = l.id_loja
+GROUP BY l.id_loja, l.nome_loja;
